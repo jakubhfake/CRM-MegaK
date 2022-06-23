ab41f90d2b79047759ef6e87f8e1fe9771ac18fb
Jakub Haliniak <jakubh.fake@gmail.com>
2022-06-23 06:19:09 +0200

    add project configuration index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const express = require('express');
-const hbs = require('express-handlebars');
+const {engine} = require('express-handlebars');
 
 const app = express();
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.static('public'));
+app.use(express.json());
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    // helpers: handlebarsHelpers,
+}));
+app.set('view engine', '.hbs');
+
+app.listen(3000, 'localhost', () => {
+    console.log('Listening on http://localhost:3000');
+})
