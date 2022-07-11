4fa277f79bb79d22d132adeba4264c3c4da93a41
Jakub Haliniak <jakubh.fake@gmail.com>
2022-07-11 08:32:02 +0200

    Add new Routers

--- views/routers/client.js
+++ views/routers/client.js
@@ -4,7 +4,19 @@ const clientRouter = express.Router();
 
 clientRouter
     .get('/', (req, res) => {
-        res.send('Dziła!');
+        res.send('Pobierz wszystkich użytkowników!');
+    })
+    .get('/:id', (req, res) => {
+        res.send('Pobierz pojedyńczdego użytkownika!');
+    })
+    .post('/', (req, res) => {
+        res.send('Dodaj użytkownika!');
+    })
+    .put('/:id', (req, res) => {
+        res.send('Zmodyfikuj użytkownika!');
+    })
+    .delete('/:id', (req, res) => {
+        res.send('Usuń użytkownika!');
     })
 
 module.exports = {
